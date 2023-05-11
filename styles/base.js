624c6a6f53cd3deec2c569fd7c0abe35eaae7604
warmgunsutisna <daclenmobile@gmail.com>
2023-05-11 18:36:36 +0700

    1177 blurhash null

--- styles/base.js
+++ styles/base.js
@@ -69,8 +69,11 @@ export const bottomNav = {
   fontSize: 7,
 };
 
+export const blurhash = null;
+/*
 export const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+  */
 
 
 
