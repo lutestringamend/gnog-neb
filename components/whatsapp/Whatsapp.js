f316d2186b6b2ef4f57314be80e422757eb0cc77
Wayne <wayne@Waynes-MacBook-Air.local>
2024-01-24 12:00:16 +0700

    fix wa number

--- components/whatsapp/Whatsapp.js
+++ components/whatsapp/Whatsapp.js
@@ -1,16 +1,18 @@
 import { Linking, Platform } from "react-native";
 
-export const openWhatsapp = (no, template) => {
+export const openWhatsapp = (no, template, forceNoTrim) => {
   let url = Platform.OS === "web" ? `https://api.whatsapp.com/send?text=${template}` : `whatsapp://send?text=${template}`;
   let number = no;
 
   if (!(number === null || number === undefined)) {
     try {
-      number = number.trim();
-      number = number.replace("0", "62");
-      number = number.replaceAll("+", "");
-      number = number.replaceAll(" ", "");
-      number = number.replaceAll("-", "");
+      if (forceNoTrim === undefined || forceNoTrim === null || !forceNoTrim) {
+        number = number.trim();
+        number = number.replace("0", "62");
+        number = number.replaceAll("+", "");
+        number = number.replaceAll(" ", "");
+        number = number.replaceAll("-", "");
+      }
       url = Platform.OS === "web" ? `https://wa.me/${number}${template ? `?text=${template}` : ""}` : `whatsapp://send${template ? `?text=${template}` : ""}}&phone=${number}`;
     } catch (e) {
       console.error(e);
